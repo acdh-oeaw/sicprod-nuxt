0cca02bffcfbb301a45b5756057834e91c9f4707
Katharina Wünsche <37192329+katharinawuensche@users.noreply.github.com>
2024-03-14 14:12:07 +0100

    Initial commit

---
+++ pages/imprint.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+defineRouteRules({
+	prerender: true,
+});
+
+definePageMeta({
+	title: "ImprintPage.meta.title",
+});
+
+const t = useTranslations();
+</script>
+
+<template>
+	<MainContent class="container grid content-start gap-y-8 py-8">
+		<PageTitle>{{ t("ImprintPage.title") }}</PageTitle>
+		<Imprint />
+	</MainContent>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+defineRouteRules({
+	prerender: true,
+});
+
+definePageMeta({
+	title: "IndexPage.meta.title",
+});
+
+const t = useTranslations();
+</script>
+
+<template>
+	<MainContent class="container grid content-start gap-y-8 py-8">
+		<PageTitle>{{ t("IndexPage.title") }}</PageTitle>
+	</MainContent>
+</template>
