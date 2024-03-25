3eb1eafccc4287e298a65a6193cd4251d96f4d30
Katharina Wünsche <katharina_wunsche@yahoo.de>
2024-03-25 09:44:11 +0100

    chore: add placeholder for person search page

---
+++ pages/search/persons.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+definePageMeta({
+	title: "Pages.searchviews.person.title",
+});
+const t = useTranslations();
+</script>
+
+<template>
+	{{ t("Pages.searchviews.person.title") }}
+</template>
