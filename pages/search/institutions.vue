75fcae969a41a1b6bbe1e7c4bc1db8d4faca52d3
Katharina Wünsche <katharina_wunsche@yahoo.de>
2024-03-27 15:55:25 +0100

    Add search view for institutions

---
+++ pages/search/institutions.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import SearchTable from "@/components/search-table.vue";
+
+definePageMeta({
+	title: "Pages.searchviews.institution.title",
+});
+const t = useTranslations();
+
+const { $api } = useNuxtApp();
+const endpoint = $api.apis_api_ontology_institution_list;
+
+const columns = [
+	{
+		key: "name",
+		label: t("Pages.searchviews.institution.name"),
+		sortable: true,
+	},
+	{
+		key: "start_date_written",
+		label: t("Pages.searchviews.institution.start_date"),
+		sortable: true,
+	},
+	{
+		key: "end_date_written",
+		label: t("Pages.searchviews.institution.end_date"),
+		sortable: true,
+	},
+	{
+		key: "type",
+		label: t("Pages.searchviews.institution.type"),
+		sortable: true,
+	},
+];
+</script>
+
+<template>
+	<SearchTable :endpoint="endpoint" :cols="columns" class-name="Institution"></SearchTable>
+</template>
