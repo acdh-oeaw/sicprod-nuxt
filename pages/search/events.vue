ea4bbc79b25208c012c8e000c3b32ca051456352
Katharina Wünsche <katharina_wunsche@yahoo.de>
2024-03-27 15:35:58 +0100

    Add event serach view

---
+++ pages/search/events.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import SearchTable from "@/components/search-table.vue";
+
+definePageMeta({
+	title: "Pages.searchviews.person.title",
+});
+const t = useTranslations();
+
+const { $api } = useNuxtApp();
+const endpoint = $api.apis_api_ontology_event_list;
+
+const columns = [
+	{
+		key: "name",
+		label: t("Pages.searchviews.event.name"),
+		sortable: true,
+	},
+	{
+		key: "start_date_written",
+		label: t("Pages.searchviews.event.start_date"),
+		sortable: true,
+	},
+	{
+		key: "end_date_written",
+		label: t("Pages.searchviews.event.end_date"),
+		sortable: true,
+	},
+	{
+		key: "type",
+		label: t("Pages.searchviews.event.type"),
+		sortable: true,
+	},
+];
+</script>
+
+<template>
+	<SearchTable :endpoint="endpoint" :cols="columns" class-name="Event"></SearchTable>
+</template>
