481c430d6f6a106c5c53676e7171fa49c52c91ff
Katharina Wünsche <katharina_wunsche@yahoo.de>
2024-03-27 16:14:23 +0100

    Add search view for places

---
+++ pages/search/places.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import SearchTable from "@/components/search-table.vue";
+
+definePageMeta({
+	title: "Pages.searchviews.place.title",
+});
+const t = useTranslations();
+
+const { $api } = useNuxtApp();
+const endpoint = $api.apis_api_ontology_place_list;
+
+const columns = [
+	{
+		key: "name",
+		label: t("Pages.searchviews.place.name"),
+		sortable: true,
+	},
+	{
+		key: "type",
+		label: t("Pages.searchviews.place.type"),
+		sortable: true,
+	},
+];
+</script>
+
+<template>
+	<SearchTable :endpoint="endpoint" :cols="columns" class-name="Place"></SearchTable>
+</template>
