fe7b382649ff76370a5831b223f5968ce6b2769e
Katharina Wünsche <katharina_wunsche@yahoo.de>
2024-04-24 11:39:00 +0200

    Make detail disclosures scrollable

--- components/detail-disclosure.vue
+++ components/detail-disclosure.vue
@@ -37,25 +37,29 @@ const localePath = useLocalePath();
 			v-if="(rels.length !== 0 && headers) || customSlot"
 			static
 			as="div"
-			class="rounded box-border overflow-hidden rounded-t-none p-4 transition-[max-height,border,padding] ui-open:max-h-screen ui-open:border-t-0 ui-not-open:max-h-0 ui-not-open:border-transparent ui-not-open:py-0"
+			class="rounded box-border overflow-hidden rounded-t-none p-4 transition-[max-height,border,padding] ui-open:max-h-full ui-open:border-t-0 ui-not-open:max-h-0 ui-not-open:border-transparent ui-not-open:py-0"
 		>
 			<slot>
 				<table class="w-full table-fixed">
-					<tr>
-						<th v-for="header in headers" :key="header" class="px-2 text-start font-bold">
-							{{ t(`Relations.${model}.${header}`) }}
-						</th>
-					</tr>
-					<NuxtLink
-						v-for="hit in rels"
-						:key="String(hit)"
-						:to="localePath(`/detail/${model}/${hit.to.id}`)"
-						class="table-row border-t hover:bg-primary-50 active:bg-primary-50 md:border-t dark:hover:bg-primary-950 dark:active:bg-primary-950"
-					>
-						<td v-for="header in headers" :key="hit + header" class="p-2 text-start">
-							{{ String(get(hit, header, "")).replace(/\<.*?\>/g, "") }}
-						</td>
-					</NuxtLink>
+					<thead class="table w-full table-fixed">
+						<tr class="">
+							<th v-for="header in headers" :key="header" class="p-2 text-start font-bold">
+								{{ t(`Relations.${model}.${header}`) }}
+							</th>
+						</tr>
+					</thead>
+					<tbody class="block max-h-screen w-full table-fixed overflow-auto">
+						<NuxtLink
+							v-for="hit in rels"
+							:key="String(hit)"
+							:to="localePath(`/detail/${model}/${hit.to.id}`)"
+							class="table w-full table-fixed border-t hover:bg-primary-50 active:bg-primary-50 md:border-t dark:hover:bg-primary-950 dark:active:bg-primary-950"
+						>
+							<td v-for="header in headers" :key="hit + header" class="p-2 text-start">
+								{{ String(get(hit, header, "")).replace(/\<.*?\>/g, "") }}
+							</td>
+						</NuxtLink>
+					</tbody>
 				</table>
 			</slot>
 		</DisclosurePanel>
