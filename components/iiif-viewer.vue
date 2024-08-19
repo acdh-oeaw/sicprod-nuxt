5d135c2d0f64606c8995ea6d285663bd1fa3b68b
Katharina Wünsche <katharina_wunsche@yahoo.de>
2024-08-19 14:42:51 +0200

    fix: retrieve page labels from manifest

--- components/iiif-viewer.vue
+++ components/iiif-viewer.vue
@@ -2,29 +2,36 @@
 import "tify/dist/tify.css";
 import "tify";
 
+import type { Manifest } from "@iiif/presentation-3";
+
 const route = useRoute();
 const router = useRouter();
 
-const page = computed(() => {
-	if (typeof route.query.page === "string")
-		return parseInt([...route.query.page.matchAll(/.*-([0-9]+)r/g)][0]?.[1] ?? "");
-	else return 1;
-});
+const manifest = ref<Manifest>();
 
 onMounted(async () => {
-	let manifest;
-	if (typeof route.query.book === "string")
-		manifest = await import(`~/assets/manifests/${decodeURI(route.query.book)}.json?url`);
+	let manifestUrl;
+	const page = route.query.page;
+
+	if (typeof route.query.book === "string") {
+		const book = route.query.book.replaceAll(/\(|\)/g, "");
+		manifestUrl = await import(`~/assets/manifests/${decodeURI(book)}.json?url`);
+		manifest.value = await import(`~/assets/manifests/${decodeURI(book)}.json`);
+	}
+	if (manifest.value === undefined) return;
+	const pageIdx = manifest.value.items.findIndex((item) => item.label?.de?.[0] === page);
 	//@ts-expect-error no Tify export
 	const tify = new Tify({
 		container: "#tify",
-		manifestUrl: manifest.default,
-		pages: [page.value],
+		manifestUrl: manifestUrl.default,
+		pages: [pageIdx + 1],
 	});
 	tify.ready.then(() => {
 		tify.viewer.addHandler("open", () => {
 			const page = tify.options.pages[0] ?? 1;
-			void router.replace({ query: { page: `${page - 1}v-${page}r`, book: route.query.book } });
+			void router.replace({
+				query: { page: manifest.value?.items[page - 1]?.label?.de?.[0], book: route.query.book },
+			});
 		});
 	});
 });
