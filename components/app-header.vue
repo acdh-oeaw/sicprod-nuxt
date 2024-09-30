2d7d56bce110c50be4a0aa06359f1530e0fa9763
Katharina Wünsche <katharina_wunsche@yahoo.de>
2024-09-30 14:14:29 +0200

    chore: rearrange navigation items

--- components/app-header.vue
+++ components/app-header.vue
@@ -5,11 +5,23 @@ const t = useTranslations();
 
 const links = computed(() => {
 	return {
-		home: { href: { path: "/" }, label: t("AppHeader.links.home") },
-		search: { href: { path: "/search/persons" }, label: t("AppHeader.links.search") },
-		scans: { href: { path: "/iiif" }, label: t("AppHeader.links.scans") },
-		network: { href: { path: "/network" }, label: t("AppHeader.links.network") },
-	} satisfies Record<string, { href: NavLinkProps["href"]; label: string }>;
+		home: { href: { path: "/" }, label: t("AppHeader.links.home"), title: "" },
+		search: {
+			href: { path: "/search/persons" },
+			label: t("AppHeader.links.search"),
+			title: t("AppHeader.links.search"),
+		},
+		network: {
+			href: { path: "/network" },
+			label: t("AppHeader.links.network"),
+			title: t("AppHeader.linkTitle.network"),
+		},
+		scans: {
+			href: { path: "/iiif" },
+			label: t("AppHeader.links.scans"),
+			title: t("AppHeader.linkTitle.scans"),
+		},
+	} satisfies Record<string, { href: NavLinkProps["href"]; label: string; title?: string }>;
 });
 </script>
 
@@ -20,7 +32,7 @@ const links = computed(() => {
 		<div class="flex items-center justify-between gap-4 p-6 md:p-8">
 			<nav :aria-label="t('AppHeader.navigation-main')">
 				<ul class="flex items-center gap-4" role="list">
-					<li v-for="(link, key) of links" :key="key">
+					<li v-for="(link, key) of links" :key="key" :title="link.title ?? ''">
 						<NavLink :href="link.href">
 							{{ link.label }}
 						</NavLink>
